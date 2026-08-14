b2ec465b7354032504566c8cc8ca9648a4c6ac6e
john <brandon.nemeth01@gmail.com>
2026-08-13 21:42:17 -0700

    Fork class added with synchronization

--- CenadeFilosofos/cena.cs
+++ CenadeFilosofos/cena.cs
@@ -1,13 +1,35 @@
-/*
- * Tarea - Cena de Filosofos
- */
- 
 using System;
+using System.Threading;
 
 namespace CenadeFilosofos {
+
+    class Fork {
+        public int Id { get; }
+        private readonly object _lock = new object();
+
+        public Fork(int id) {
+            Id = id;
+        }
+
+        public void PickUp(string philosopherName) {
+            Monitor.Enter(_lock);
+            Console.WriteLine($"  {philosopherName} toma el tenedor {Id}");
+        }
+
+        public void PutDown(string philosopherName) {
+            Console.WriteLine($"  {philosopherName} suelta el tenedor {Id}");
+            Monitor.Exit(_lock);
+        }
+    }
+    class MainApplication {
+        public static void Run() {}
+
+    }
+
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Testing env");
+            Console.WriteLine("=== La Cena de los Filosofos ===");
+            MainApplication.Run();
         }
     }
 }
